bc5681214135c6cff32ad1ce9b61af38aee87536
Sean Langton <slangton1@gmail.com>
2019-01-28 21:17:01 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,10 +2,13 @@ class Dog
 
   attr_accessor :name, :breed, :age
 
-  @all = []
+  @@all = []
 
-  def initialize
-    self << @all
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+    self << @@all
   end
 
   def self.all
